596b7e281718b0035ac7d8f46df001e215587b72
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2022-02-22 11:23:56 +0200

    Fix and extend unit tests
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

--- spec/defines/db_spec.rb
+++ spec/defines/db_spec.rb
@@ -2,10 +2,15 @@
 
 require 'spec_helper'
 
-describe 'pimcore::params' do
+describe 'pimcore::db' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:title) { 'pimcore' }
       let(:facts) { os_facts }
+      let(:params) do
+        { 'user'     => 'pimcore',
+          'password' => 'foobar', }
+      end
 
       it { is_expected.to compile }
     end
